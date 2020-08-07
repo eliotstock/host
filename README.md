a44b3a86df735b9b2ac5a153ac5c127c6ee4f4e1
Eliot Stock <eliotstock@google.com>
2020-08-07 15:47:05 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # linux-setup
-Small script to set up a new Linux desktop/laptop installation
+
+Small script to set up a new Linux desktop/laptop
+
+## Read this much from the old machine
+
+* Go for the minimal installation, plus 3P software
+* Add a Google account straight out of setup. This puts a mountable network drive into the file manager.
+* Open Firefox, search “Chrome download” and download the .deb file. Save it, don't open it with the archive manager. 
+* `sudo apt install ./google-chrome*`
+* This also sets up the Google repository for Chrome. Find Chrome in all apps and add it to the favourites.
+* Open Chrome and go to [this repo on Github](https://github.com/eliotstock/linux-setup)
+
+## Now switch to the new machine
+
+* `git clone https://github.com/eliotstock/linux-setup`
+* `cd linux-setup`
+* `chmod 755 ./setup.sh`
+* `./setup.sh`
+* Fix any errors in the script and push them up for next time.
---
+++ setup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Add some packages and snaps
+# Dev
+sudo apt-get install -y git
+sudo apt-get install -y emacs
+sudo apt-get install -y virtualenv
+sudo apt-get install -y gnome-tweaks
+sudo snap install android-studio --classic
+sudo snap install vscode --classic
+sudo snap install node --classic --channel=8/stable
+# Other
+sudo snap install slack --classic
+sudo snap install spotify
+sudo snap install vlc
+sudo apt-get install -y simple-scan
+# Required by hp-setup GUI
+sudo apt-get install -y python-qt4
+sudo apt-get install -y python3-pip
+pip3 install pyqt5
+
+# Gnome settings
+gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
+gsettings set org.gnome.desktop.background picture-uri \
+ 'file:////usr/share/gnome-control-center/pixmaps/noise-texture-light.png'
+gsettings set org.gnome.desktop.screensaver picture-uri \
+ 'file:////usr/share/gnome-control-center/pixmaps/noise-texture-light.png'
+gsettings set org.gnome.desktop.peripherals.mouse speed -0.15
+gsettings set org.gnome.desktop.interface.show-battery-percentage true
+gsettings set org.gnome.nautilus.preferences.default-folder-viewer 'list-view'
+gsettings set org.gnome.nautilus.list-view.default-zoom-level 'small'
+gsettings set org.gnome.shell.extensions.dash-to-dock.dash-max-icon-size
+  20
+
+# Increase the number of watches available to inotify for IDEs
+sudo echo "fs.inotify.max_user_watches = 524288" > /etc/sysctl.d/50-eliot_more_watches.conf
