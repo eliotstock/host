10d1ee55fdc8445eddf2636e504f16f865585b7a
Eliot Stock <1@eliotstock.com>
2020-08-07 17:34:37 +0100

    touchpad speed

--- setup.sh
+++ setup.sh
@@ -26,6 +26,7 @@ gsettings set org.gnome.desktop.peripherals.mouse.natural-scroll false
 gsettings set org.gnome.desktop.background picture-uri 'file:////usr/share/gnome-control-center/pixmaps/noise-texture-light.png'
 gsettings set org.gnome.desktop.screensaver picture-uri 'file:////usr/share/gnome-control-center/pixmaps/noise-texture-light.png'
 gsettings set org.gnome.desktop.peripherals.mouse speed -0.15
+gsettings set org.gnome.desktop.peripherals.touchpad speed 0.60
 gsettings set org.gnome.shell.extensions.dash-to-dock.dash-max-icon-size 20
 
 # These fail on Ubutnu 20.04, but the keys and values are correct
