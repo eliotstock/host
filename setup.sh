4e5fc60afe8bbeb1a48222ae4b0b80a617e7a24a
Eliot Stock <1@eliotstock.com>
2020-08-09 07:49:02 +0100

    Fix syntax for gsettings calls.

--- setup.sh
+++ setup.sh
@@ -23,19 +23,17 @@ sudo apt-get install -y fprintd
 
 # Gnome settings
 gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
-gsettings set org.gnome.desktop.peripherals.touchpad.natural-scroll false
-gsettings set org.gnome.desktop.peripherals.mouse.natural-scroll false
+gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll false
+gsettings set org.gnome.desktop.peripherals.mouse natural-scroll false
 gsettings set org.gnome.desktop.background picture-uri 'file:////usr/share/gnome-control-center/pixmaps/noise-texture-light.png'
 gsettings set org.gnome.desktop.screensaver picture-uri 'file:////usr/share/gnome-control-center/pixmaps/noise-texture-light.png'
 gsettings set org.gnome.desktop.peripherals.mouse speed -0.15
 gsettings set org.gnome.desktop.peripherals.touchpad speed 0.60
 gsettings set org.gnome.shell.extensions.dash-to-dock.dash-max-icon-size 20
 gsettings set org.gnome.shell.extensions.dash-to-dock.dock-fixed false
-
-# These fail on Ubutnu 20.04, but the keys and values are correct
-gsettings set org.gnome.desktop.interface.show-battery-percentage true
-gsettings set org.gnome.nautilus.preferences.default-folder-viewer 'list-view'
-gsettings set org.gnome.nautilus.list-view.default-zoom-level 'small'
+gsettings set org.gnome.desktop.interface show-battery-percentage true
+gsettings set org.gnome.nautilus.preferences default-folder-viewer 'list-view'
+gsettings set org.gnome.nautilus.list-view default-zoom-level 'small'
 
 # Increase the number of watches available to inotify for IDEs
 sudo echo "fs.inotify.max_user_watches = 524288" > /etc/sysctl.d/50-eliot_more_watches.conf
